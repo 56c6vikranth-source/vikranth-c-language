9cfe7b7c455ff1f5adb9d43317fd56508f2ea703
N Sai Vikranth Goud <56c6vikranth@gmail.com>
2025-11-14 11:47:00 +0530

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conic.h>
+int main()
+{
+     int num1,num2,sum;
+     printf("enter two integers:");
+     scanf(%d,&num1);
+     printf(enter two integer:");
+     scanf(%d,&num2);
+     sum=num1+num2;
+     printf("sum: %d,sum);
+     gtech();
+     return 0;
+ }
